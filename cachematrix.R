e01352de9efca34d351dfa2dac03c8c90490faa8
alx852 <alx@xmail.net>
2015-05-24 19:02:08 +0200

    commit assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,30 @@
-makeCacheMatrix <- function(x = matrix()) {
+# Creates a matrix and caches its inverse
 
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+# Calculates the inverse of a matrix
+# If the inverse has already been calculated, it returns the cache
+
 cacheSolve <- function(x, ...) {
-     
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
